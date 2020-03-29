76559d7027ff06c5918b7a8e899373c14abf4c75
wintermaples <pg.wintermaples@gmail.com>
2020-03-30 00:00:12 +0900

    Created custom user model.

--- suguvote_root/settings.py
+++ suguvote_root/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'suguvote.apps.SuguvoteConfig',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.User'
