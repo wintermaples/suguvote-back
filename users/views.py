194dac004c18acd6e9d26a3742d500159a5d4e0c
wintermaples <pg.wintermaples@gmail.com>
2020-03-30 22:42:00 +0900

    Deleted list of /users/. When deleting user, the user will be inactivated instead of deleting from DB.

--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from users.models import User
 from users.serializers import UserRetrieveSerializer, UserCreateSerializer, UserUpdateSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet):
     queryset = User.objects.all()
 
     def get_serializer_context(self):
@@ -18,3 +25,14 @@ class UserViewSet(viewsets.ModelViewSet):
             'PUT': UserUpdateSerializer,
             'PATCH': UserUpdateSerializer,
         }[self.request.method]
+
+    @action(detail=False)
+    def me(self, request):
+        if request.user is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(self.get_serializer(request.user).data)
+
+    def perform_destroy(self, instance: User):
+        instance.is_active = False
+        instance.save()
