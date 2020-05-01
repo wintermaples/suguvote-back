99f2f6371427031606e8e8d26a12bf28b70e9c9d
wintermaples <pg.wintermaples@gmail.com>
2020-05-01 11:46:50 +0900

    Revert "Fix recaptcha.py"
    This reverts commit 3123f0d9

--- common/recaptcha.py
+++ common/recaptcha.py
@@ -1,25 +1,11 @@
 # -*- coding: utf-8 -*-
-import json
-import traceback
-
 from django.conf import settings
 import requests
 
 
 def verify_recaptcha(token: str) -> bool:
-    '''
-    与えられたTokenから、ReCAPTCHAの成功/失敗を返します。
-    何らかのエラーが発生した場合は、エラーを出力してFalseを返します。
-
-    :param token: ReCAPTCHAのClient-Side側APIを叩いたときに取得したToken
-    :return: ReCAPTCHAが成功している場合はTrue
-    '''
-    try:
-        response = requests.post('https://www.google.com/recaptcha/api/siteverify', {
-            'secret': settings.RECAPTCHA_SECRET_KEY,
-            'response': token
-        })
-        return bool(json.loads(response.text)['success'])
-    except Exception as ex:
-        traceback.print_exc(ex)
-        return False
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', {
+        'secret': settings.RECAPTCHA_SECRET_KEY,
+        'response': token
+    })
+    return bool(response.content)
