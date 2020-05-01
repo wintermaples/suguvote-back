c4cf5036c6d4dee217a25a6e1235a57db4913171
wintermaples <pg.wintermaples@gmail.com>
2020-05-01 11:47:29 +0900

    Fix recaptcha. close #14

--- common/recaptcha.py
+++ common/recaptcha.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
+import json
+import traceback
+
 from django.conf import settings
 import requests
 
 
 def verify_recaptcha(token: str) -> bool:
-    response = requests.post('https://www.google.com/recaptcha/api/siteverify', {
-        'secret': settings.RECAPTCHA_SECRET_KEY,
-        'response': token
-    })
-    return bool(response.content)
+    '''
+    与えられたTokenから、ReCAPTCHAの成功/失敗を返します。
+    何らかのエラーが発生した場合は、エラーを出力してFalseを返します。
+
+    :param token: ReCAPTCHAのClient-Side側APIを叩いたときに取得したToken
+    :return: ReCAPTCHAが成功している場合はTrue
+    '''
+    try:
+        response = requests.post('https://www.google.com/recaptcha/api/siteverify', {
+            'secret': settings.RECAPTCHA_SECRET_KEY,
+            'response': token
+        })
+        return bool(json.loads(response.text)['success'])
+    except Exception as ex:
+        traceback.print_exc(ex)
+        return False
