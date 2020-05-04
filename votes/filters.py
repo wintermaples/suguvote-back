436523a89c2182d46644307861e08620f0d0d09f
wintermaples <pg.wintermaples@gmail.com>
2020-05-04 16:00:09 +0900

    Implemented #9.

---
+++ votes/filters.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from django_filters import rest_framework as filters
+
+from votes.models import Vote
+
+
+class VoteFilter(filters.FilterSet):
+    like = filters.CharFilter(method='like_filter', label='Like')
+    ordering = filters.OrderingFilter(
+        fields=('created_at', 'vote_count')
+    )
+
+    def like_filter(self, qs, name, value):
+        return qs.filter(
+            Q(title__icontains=value) | Q(description__icontains=value)
+        )
+
+    class Meta:
+        model = Vote
+        fields = []
