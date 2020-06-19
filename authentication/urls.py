4f1a863d39d5d19b0f764d9c3bb17c74f6314a73
wintermaples <pg.wintermaples@gmail.com>
2020-06-20 04:28:02 +0900

    Add: logout and is_logged_in view.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from authentication.views import login_view
+from authentication.views import login_view, logout_view, is_logged_in_view
 
 urlpatterns = [
-    path('login/', login_view)
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('is_logged_in/', is_logged_in_view),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,9 +9,22 @@ from users.models import User
 
 @api_view(['POST'])
 def login_view(request):
+    if 'username' not in request.data or 'password' not in request.data:
+        return Response(status=400)
     user: User = authenticate(username=request.data['username'], password=request.data['password'])
     if user is not None:
         login(request, user)
         return Response(status=200)
 
     return Response(status=401)
+
+
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return Response(status=200)
+
+
+@api_view(['GET'])
+def is_logged_in_view(request):
+    return Response(status=200, data={'is_logged_in': (not request.user.is_anonymous)})
